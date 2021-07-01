32c945ff5ff7ab68ed9443fa02f507111035f6e5
codeSmith13 <peeperz13@gmail.com>
2021-07-01 15:52:36 -0500

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React,{ useState, useEffect} from 'react';
 import Greeter from './components/Greeter';
 
 
+let App = () => {
+    const [username, setUsername] = useState('');
+    const [loaded, setLoaded] = useState(false);
 
+     useEffect(() => {
+    if (loaded === false) {
+      setTimeout(() => setLoaded(!loaded), 3000);
+    }
+  }, [loaded]);
 
+  let handleClick = () => setLoaded(!loaded);
 
-const App = () => {
+  if (loaded) {
+
     return (
         <>
             < Greeter phrase="Whats up" name="Chris"/>
             < Greeter phrase="Whats up" name="Cody"/>
             < Greeter phrase="Whats up" name="Brad" />
-            <input type="text" onChange={(e) => (e.target.value)} />
-            <p>You are logging in as: </p>
+            <input type="text" placeholder="Type your username" value={username} onChange={(e) => setUsername(e.target.value)} />
+            <p>You are logging in as:{username} </p>
              <p></p>
         </>
     );
+     } else {
+    return (
+      <>
+        <h1>Loading...</h1>
+        <button onClick={() => handleClick()}>Click to load page</button>
+      </>
+    );
+ }
+
 };
 
 export default App;
